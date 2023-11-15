2911be6c9ab70da1a9637d1726710d8e6d323bc0
Kaito-Dogi <kendobu0405@gmail.com>
2023-11-16 05:10:41 +0900

    style: Divider の太さを変更

--- src/components/ui/Divider/Divider.styles.ts
+++ src/components/ui/Divider/Divider.styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
   divider: {
     alignSelf: "stretch",
     height: 0,
-    borderWidth: 0.5,
+    borderWidth: 1,
     borderColor: colors.disabled,
     borderStyle: "dashed",
   },
